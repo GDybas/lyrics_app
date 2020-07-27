ec33eb84ea998c279b72dab6aff1f7be1bb48650
GDybas <grze.dybas@gmail.com>
2020-07-27 13:40:01 +0200

    added logo, changed colors, better layout on list and lyrics page, added js for switching pages on mobile

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,18 @@
+const listPage = document.getElementById('song-choice-section');
+const lyricsPage = document.getElementById('song-lyrics-section');
+
+//Event Listeners
+document.getElementById('songs-list').addEventListener('click', getSongLyrics);
+document.querySelector('.change-view-btn').addEventListener('click', changeView);
+
+
+
 //Displaying songs list
 function getSongList() {
   fetch("./lyrics.json")
     .then(res => res.json())
     .then(data => {
-      console.log(data);
+      //console.log(data);
       let output = "";
       data.forEach(song => {
         output += `<li class="song-btn" id="${song.id}">${song.title}</li>`;
@@ -15,8 +24,6 @@ function getSongList() {
 getSongList();
 
 
-document.getElementById('songs-list').addEventListener('click', getSongLyrics);
-
 //Wyświetlenie napisów po kliknięciu na liście
 function getSongLyrics(e) {
 
@@ -26,7 +33,7 @@ function getSongLyrics(e) {
         return res.json();
       })
       .then(function (data) {
-        console.log(data);
+        //console.log(data);
         let title = '';
         let author = '';
         let lyrics = '';
@@ -41,12 +48,19 @@ function getSongLyrics(e) {
         document.getElementById('title').innerHTML = title;
         document.getElementById('author').innerHTML = author;
         document.getElementById('lyrics').innerHTML = lyrics;
+        lyricsPage.style.display = 'flex';
+        listPage.style.display = 'none';
       })
       .catch(function (err) {
         console.log(err);
       });
   }
+}
 
-
-
+function changeView(e) {
+  if (e.target.id === 'to-list') {
+    console.log(listPage);
+    lyricsPage.style.display = 'none';
+    listPage.style.display = 'flex';
+  }
 }
